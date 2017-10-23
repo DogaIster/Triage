ce91bcac275d82fabc031a53610f38a3fe3cef48
Doga Ister <dogaister@Dogas-MacBook-Pro.local>
2017-10-22 20:59:38 -0400

    Encourters and adding encourters unrelated to patient

--- rubyrails5-angular2-app/frontend/src/app/encourter-add/encourter-add.component.ts
+++ rubyrails5-angular2-app/frontend/src/app/encourter-add/encourter-add.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Encourter } from "../encourter";
+import { ApiService } from "../api.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-encourter-add',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncourterAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(public apiService: ApiService , public acRoute : ActivatedRoute) { }
+
+  public encourter : Encourter = new Encourter();
 
   ngOnInit() {
+
+    this.acRoute.params.subscribe((data : any)=>{
+      console.log(data.id);
+      if(data && data.id){
+        this.apiService.get("encourters/"+data.id).subscribe((data : Encourter)=>{
+          this.encourter = data;
+        });
+      }
+      else
+      {
+        this.encourter = new Encourter();
+      }
+    })
   }
+
+  public onSubmit(){
+    console.log("Adding a product: " + this.encourter.patientID);
+    /* if(this.patient.patientID){
+       this.apiService.update("patients/"+this.patient.patientID,this.patient).subscribe((r)=>{
+         console.log(r);
+         alert("Patient updated !");
+       })
+     }*/
+
+    this.apiService.post("encourters", this.encourter).subscribe((r) => {
+      console.log(r);
+      this.encourter = new Encourter();
+      alert("Encourter Record added !");
+      window.location.reload();
+    });
+  }
+
 
 }
--- rubyrails5-angular2-app/frontend/src/app/encourter-list/encourter-list.component.ts
+++ rubyrails5-angular2-app/frontend/src/app/encourter-list/encourter-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from "@angular/router";
+import { ApiService } from "../api.service";
+import { Encourter } from "../encourter";
 
 @Component({
   selector: 'app-encourter-list',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncourterListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public apiService: ApiService , public router : Router) { }
+
+  public columns = Array<Encourter>();
 
   ngOnInit() {
+    this.apiService.get("encourters").subscribe((data : Encourter[])=>{
+      console.log(data);
+      this.columns = data;
+    });
   }
 
+
+  public destroy(id:string){
+    console.log("delete : " + id);
+    var path = 'encourters/' + id;
+    this.apiService.delete(path).subscribe((r)=>{
+
+      this.columns = this.columns.filter((p,i)=>{
+
+        if(Number(id) === p.encourterID )
+        {
+          return false;
+        }
+        return true;
+      },this.columns)
+      window.location.reload();
+    });
+
+
+  }
+  public edit(id:string){
+    console.log("update : " + id );
+    this.router.navigateByUrl('/encourters/add/' + id);
+    window.location.reload();
+  }
 }
